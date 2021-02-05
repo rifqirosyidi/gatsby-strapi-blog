dd3c3cdff45570f94c2e57b26d18ce8d6589ac27
Rifqi Rosyidi <rief.rosyidi@gmail.com>
2021-02-05 15:40:38 +0700

    initial commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ca4d938a1896bf826930e6792bc4fa47'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ac5c2e4d-4617-44b2-8149-b0c3cb2c6b90'
+};
